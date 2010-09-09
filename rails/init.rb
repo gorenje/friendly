cec4c7c6c35167962ad741a92d5e755ef5a21fb6
Gerrit Riessen <gerrit.riessen@gmail.com>
2010-09-09 11:33:16 +0200

    Adjusted the placement of brackets in the init.rb

--- rails/init.rb
+++ rails/init.rb
@@ -1,6 +1,6 @@
 configuration_path = RAILS_ROOT + "/config/friendly.yml"
 if File.exists?(configuration_path)
-  config = YAML.load(ERB.new(File.read(configuration_path).result))[RAILS_ENV]
+  config = YAML.load(ERB.new(File.read(configuration_path)).result)[RAILS_ENV]
   Friendly.configure(config)
 end
 
